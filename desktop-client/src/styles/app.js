4f5fb8c3197c9970b22e33df94887f3a166c43fa
Elijah Masanga <masanga@lisa.africa>
2022-01-03 16:22:08 +0300

    Mac Address and Geo Location Setup

--- desktop-client/src/styles/app.js
+++ desktop-client/src/styles/app.js
@@ -12,6 +12,73 @@ function setupClientGateway() {
 
 }
 
+//Get Next Page
+function getGoNextPage(scoreFactorIdsList) {
+
+	var next_page = ""
+
+	var fileName = location.href.split("/").slice(-1); 
+
+	//Detemine Next Page for Verification / Validation
+	if (scoreFactorIdsList != 0) {
+
+		if (scoreFactorIdsList[0] == 2) {
+			next_page = "mac_address_verification.html";	
+
+		}else if (scoreFactorIdsList[0] == 3) {
+			next_page = "geo_location_verification.html";
+
+		}else if (scoreFactorIdsList[0] == 4) {
+			next_page = "tpm_security_check.html";
+
+		}else if (scoreFactorIdsList[0] == 5) {
+			next_page = "face_recognition_verification.html";
+
+		}
+
+	}else {
+		next_page = "client_dashboard.html"
+	}
+
+	if (fileName == next_page) {
+		next_page = "client_dashboard.html"
+	}
+
+	return next_page;
+}
+
+
+//Continue Button
+function continueBtnNext() {
+
+	axios.get(apiServerUrl + "/api/v1/get/next/page/client/continue", { 
+            headers : {
+                'Content-Type': 'application/json',
+                clientId: localStorage.getItem('clientId'),
+                clientToken: localStorage.getItem('clientToken')
+            }
+
+        }).then(function (response) {
+
+        	const resData = response.data
+
+        	const jsonData = JSON.parse(JSON.stringify(resData))
+
+        	if (jsonData['status'] == true) {
+
+        		var scoreFactorIdsList = jsonData['scoreFactorIdsList']
+
+        		//Next Page
+        		var next_page = getGoNextPage(scoreFactorIdsList)
+
+        		window.location.assign(next_page)
+
+    		}
+
+    	})
+
+}
+
 //Get Client Public Ip
 function getClientPublicIp() {
 	$.get('https://api.ipify.org?format=json', function (data) {
